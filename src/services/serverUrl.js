5bdddf5fbe58faa2eb86e3638f67eac9a1aeb03d
Your Name <sanjukjoseph1610@gmail.com>
2024-07-28 22:33:41 +0530

    Server URL setup

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = "http://localhost:3000"
-// export const SERVER_URL = "https://tech-hire-server.onrender.com"
+// export const SERVER_URL = "http://localhost:3000"
+export const SERVER_URL = "https://dealsdray-server-5prt.onrender.com"
